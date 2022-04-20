2219686c0fef39f779a36b8d500d7e226ddcd7e6
PR070HAX0r <onlyboomm@gmail.com>
2022-04-20 11:19:57 -0400

    Need to update struct with strings

--- sudoku/board/board.go
+++ sudoku/board/board.go
@@ -1,12 +1,14 @@
 package board
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 )
 
 const (
-	Cols = 9
-	Rows = 9
+	cols = 9
+	rows = 9
 	n1   = "\n"
 )
 
@@ -23,51 +25,45 @@ type Board struct {
 	BottomLeft              string // ┗
 	HorizontalBoxSeparator  string // ━━━
 	HorizontalRowSeparator  string // ─
-	LeftBoxSeparator        string // ┠
+	LeftBoxSeparator        string // ┣
+	LeftRowSeparator        string // ┠
 	RightBoxSeparator       string // ┫
+	RightRowSeparator       string // ┫
 	MiddleBoxSeparator      string // ╋
 	MiddleColumnSeparator   string // "┼"
 	MiddleHybridSeparator   string // ┿
 }
 
-func (b Board) toString() {
+func (b Board) printBoard() {
 	TopBox := strings.Repeat(b.HorizontalBoxSeparator+b.BottonColumnSeparator, 2) + b.HorizontalBoxSeparator + b.TopBoxSeparator
-	TopBar := b.TopLeft + TopBox + b.TopRight
+	TopBar := b.TopLeft + TopBox + b.TopRight + n1
+	RowBox := strings.Repeat(b.HorizontalRowSeparator, 3)
+	BoxHorizontal := b.LeftBoxSeparator + b.HorizontalBoxSeparator + b.MiddleBoxSeparator
 	BottomBox := strings.Repeat(b.HorizontalBoxSeparator+b.TopColumnSeparator, 2) + b.HorizontalBoxSeparator + b.BottomBoxSeparator
-	BottomBar := b.BottomLeft + BottomBox + b.BottomRight
-
-	// run for loop on NumberBox to input xy of matrix if xy is null then ""
-	// Number := "" + "xy" + ""
-
-	// NumberRow := b.VerticalBoxSeparator + NumberBox  + b.VerticalBoxSeparator
-	// SeparatorRow :=
+	BottomBar := b.BottomLeft + BottomBox + b.BottomRight + n1
+	fmt.Printf("%s", TopBar)
+	for i := 0; i < rows; i++ {
+		completerow := getNum(i) + n1
+		rowSeparator := b.LeftRowSeparator + RowBox + b.VerticalBoxSeparator + n1
+		if i == 3 {
+			fmt.Printf(BoxHorizontal)
+		}
+		fmt.Printf(completerow)
+		fmt.Printf(rowSeparator)
+	}
+	fmt.Printf("%s", BottomBar)
 }
 
-// for i := 0; i < boardSize; i++ {
-// 	for j := 0; j < boardSize; j++ {
-// 		num := state.board[i][j]
-// 		if num == 0 {
-// 			fmt.Print("    ")
-// 		} else {
-// 			if enableColor {
-// 				fmt.Printf("\033[3%sm%4d\033[0m", colorPalette[int(math.Log2(float64(num)))], num)
-// 			} else {
-// 				fmt.Printf("%4d", num)
-// 			}
-// 		}
-// 		if j != boardSize-1 {
-// 			fmt.Print("|")
-// 		}
-// 	}
-// 	fmt.Print("\n")
-// 	if i != boardSize-1 {
-// 		for j := 0; j < boardSize; j++ {
-// 			if j != boardSize-1 {
-// 				fmt.Print("-----")
-// 			} else {
-// 				fmt.Print("----")
-// 			}
-// 		}
-// 		fmt.Print("\n")
-// 	}
-// }
+func (b Board) getNum(row int) string {
+	var numbers []int
+	i := row
+	for j := 0; j < cols; j++ {
+		num, err := state.game[i][j]
+		numbers[j] = num
+	}
+	Box1 := " " + strconv.Itoa(numbers[0]) + " " + b.VerticalColumnSeparator + " " + strconv.Itoa(numbers[1]) + " " + b.VerticalColumnSeparator + " " + strconv.Itoa(numbers[2]) + " " + b.VerticalColumnSeparator
+	Box2 := " " + strconv.Itoa(numbers[3]) + " " + b.VerticalColumnSeparator + " " + strconv.Itoa(numbers[4]) + " " + b.VerticalColumnSeparator + " " + strconv.Itoa(numbers[5]) + " " + b.VerticalColumnSeparator
+	Box3 := " " + strconv.Itoa(numbers[6]) + " " + b.VerticalColumnSeparator + " " + strconv.Itoa(numbers[7]) + " " + b.VerticalColumnSeparator + " " + strconv.Itoa(numbers[8]) + " "
+	completerow := b.VerticalBoxSeparator + Box1 + Box2 + Box3 + b.VerticalBoxSeparator
+	return completerow
+}
