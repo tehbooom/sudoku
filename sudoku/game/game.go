e1bf1eb45af5c81b627380c944db8a0ca9cd68ab
PR070HAX0r <onlyboomm@gmail.com>
2022-05-14 11:20:07 -0400

    Read the TODO

--- sudoku/game/game.go
+++
@@ -1,6 +0,0 @@
-package game
-
-func state() {
-
-	//here we put the state of the game in a martix
-}
--- sudoku/main.go
+++ sudoku/main.go
@@ -4,19 +4,23 @@ import (
 	"fmt"
 	"os"
 
+	_ "github.com/tehbooom/sudoku/board"
+
 	"github.com/mbndr/figlet4go"
 )
 
 func main() {
+	state := board.State{}
 	ascii := figlet4go.NewAsciiRender()
 	renderStr, _ := ascii.Render("Sudoku")
 	fmt.Print(renderStr)
-	fmt.Printf("Would you like to play sudoku? (y/n)\n")
 
+	// check if the player wants to play the game
+	fmt.Printf("Would you like to play sudoku? (y/n)\n")
 	var game string
 	fmt.Scanln(&game)
 	if game == "y" {
-
+		board.generateEmpty()
 	} else if game == "n" {
 		fmt.Printf("Goodbye!\n")
 		os.Exit(0)
@@ -24,18 +28,38 @@ func main() {
 		fmt.Printf("Please enter y or n\n")
 		os.Exit(0)
 	}
+	fmt.Printf("Generating Puzzle...\n")
+	board.start()
 
-	var difficulty string
-	fmt.Printf("What difficulty would you like to play? (easy/medium/hard)\n")
-	fmt.Scanln(&difficulty)
-	if difficulty == "easy" {
-		fmt.Printf("Test\n")
-	} else if difficulty == "medium" {
-		fmt.Printf("Test\n")
-	} else if difficulty == "hard" {
-		fmt.Printf("Test\n")
-	} else {
-		fmt.Printf("Please enter easy medium or hard\n")
-		os.Exit(0)
+	// start infinite game loop
+	for {
+		fmt.Print(renderStr)
+
+		board.PrintBoard()
+		fmt.Println("Please enter the row column and value to put on the board..\n")
+		fmt.Println("For example, 1 2 9")
+		for {
+			var row, column, value int
+			fmt.Scan(&row, &column, &value)
+			valid := board.placeNumber(row, column, value)
+
+			if valid == nil {
+				break
+			}
+			fmt.Println(valid)
+			fmt.Printf("Please re-enter a correct position and value...\n")
+		}
+
+		result = board.checkForWinner()
+		if result == completeWrong {
+			fmt.Println("Somethings not right check again..")
+		}
+		if result == winner {
+			break
+		}
+		if result == incomplete {
+			fmt.Println()
+		}
 	}
+
 }
