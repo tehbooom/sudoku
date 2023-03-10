66e2718971998e35c513b3a9f49c1bc229206d3a
PR070HAX0r <onlyboomm@gmail.com>
2023-03-10 18:00:58 -0500

    i suk

--- board/board.go
+++ board/board.go
@@ -2,38 +2,62 @@ package board
 
 import (
 	"fmt"
-	"math/rand"
 	"strconv"
 	"strings"
-	"time"
-	"github.com/tehbooom/sudoku/style"
+
 	"github.com/tehbooom/sudoku/game"
+	_ "github.com/tehbooom/sudoku/style"
 )
 
+const (
+	cols                    = 9
+	rows                    = 9
+	n1                      = "\n"
+	topRight                = "┓"
+	topBoxSeparator         = "┳"
+	topColumnSeparator      = "┯"
+	topLeft                 = "┏"
+	verticalBoxSeparator    = "┃"
+	verticalColumnSeparator = "│"
+	bottomRight             = "┛"
+	bottomBoxSeparator      = "┻"
+	bottomColumnSeparator   = "┷"
+	bottomLeft              = "┗"
+	horizontalBoxSeparator  = "━━━"
+	horizontalRowSeparator  = "───"
+	leftBoxSeparator        = "┣"
+	leftRowSeparator        = "┠"
+	rightBoxSeparator       = "┫"
+	rightRowSeparator       = "┫"
+	middleBoxSeparator      = "╋"
+	middleColumnSeparator   = "┼"
+	middleHybridSeparator   = "┿"
+)
+
+// prints the sudoku board with all values from game
 func PrintBoard() {
-	topBox := strings.Repeat(style.horizontalBoxSeparator+style.topColumnSeparator, 2) + style.horizontalBoxSeparator + style.topBoxSeparator
-	var top string = style.topLeft + strings.Repeat(topBox, 2) + strings.Repeat(style.horizontalBoxSeparator + style.topColumnSeparator, 2) + style.horizontalBoxSeparator + style.topRight + style.n1
+	topBox := strings.Repeat(horizontalBoxSeparator+topColumnSeparator, 2) + horizontalBoxSeparator + topBoxSeparator
+	var top string = topLeft + strings.Repeat(topBox, 2) + strings.Repeat(horizontalBoxSeparator+topColumnSeparator, 2) + horizontalBoxSeparator + topRight + n1
 	// ┏━━━┯━━━┯━━━┳━━━┯━━━┯━━━┳━━━┯━━━┯━━━┓
 
-	bottomBox := strings.Repeat(style.horizontalBoxSeparator+style.bottomColumnSeparator, 2) + style.horizontalBoxSeparator + style.bottomBoxSeparator
-	var bottom string = style.bottomLeft + strings.Repeat(bottomBox, 2) + strings.Repeat(style.horizontalBoxSeparator + style.bottomColumnSeparator, 2) + style.horizontalBoxSeparator + style.bottomRight + style.n1
+	bottomBox := strings.Repeat(horizontalBoxSeparator+bottomColumnSeparator, 2) + horizontalBoxSeparator + bottomBoxSeparator
+	var bottom string = bottomLeft + strings.Repeat(bottomBox, 2) + strings.Repeat(horizontalBoxSeparator+bottomColumnSeparator, 2) + horizontalBoxSeparator + bottomRight + n1
 	// ┗━━━┷━━━┷━━━┻━━━┷━━━┷━━━┻━━━┷━━━┷━━━┛
 
-
-	rowColumn := strings.Repeat(style.horizontalRowSeparator + style.middleColumnSeparator, 2) + style.horizontalRowSeparator + style.verticalBoxSeparator
-	rowSeparator := style.leftRowSeparator + strings.Repeat(rowColumn, 2) + strings.Repeat(style.horizontalRowSeparator + style.middleHybridSeparator, 2)  + style.horizontalRowSeparator + style.rightRowSeparator
+	rowColumn := strings.Repeat(horizontalRowSeparator+middleColumnSeparator, 2) + horizontalRowSeparator + verticalBoxSeparator
+	rowSeparator := leftRowSeparator + strings.Repeat(rowColumn, 2) + strings.Repeat(horizontalRowSeparator+middleHybridSeparator, 2) + horizontalRowSeparator + rightRowSeparator
 	// ┠───┼───┼───╂───┼───┼───╂───┼───┼───┨
 
-	rowBox := strings.Repeat(style.horizontalBoxSeparator + style.middleHybridSeparator, 2) + style.horizontalBoxSeparator + style.verticalBoxSeparator
-	boxSeparator := style.leftBoxSeparator + strings.Repeat(rowBox, 2) + strings.Repeat(style.horizontalBoxSeparator + style.middleBoxSeparator, 2) + style.horizontalBoxSeparator + style.rightBoxSeparator
+	rowBox := strings.Repeat(horizontalBoxSeparator+middleHybridSeparator, 2) + horizontalBoxSeparator + verticalBoxSeparator
+	boxSeparator := leftBoxSeparator + strings.Repeat(rowBox, 2) + strings.Repeat(horizontalBoxSeparator+middleBoxSeparator, 2) + horizontalBoxSeparator + rightBoxSeparator
 	// ┣━━━┿━━━┿━━━╋━━━┿━━━┿━━━╋━━━┿━━━┿━━━┫
 
 	// Creates the middle
 	// ┃ 1 │ 2 │ 3 ┃ 4 │ 5 │ 6 ┃ 7 │ 8 │ 9 ┃ * 9
 	contents := make([]string, 17)
-	for _, i := range contents{
-		if i != 0 && i % 2 != 0 { // all odd numbers for contents except 0
-			if i == 5 || 11 { // the Box separator only appears twice in board
+	for i := range contents {
+		if i != 0 && i%2 != 0 { // all odd numbers for contents except 0
+			if i == 5 || i == 11 { // the Box separator only appears twice in board
 				contents[i] = boxSeparator
 			}
 			contents[i] = rowSeparator
@@ -45,35 +69,30 @@ func PrintBoard() {
 			var row string = getNum(i)
 			contents[newIndex] = row
 		}
-		var row string = s.getNum(i)
+		var row string = getNum(i)
 		contents[0] = row
 	}
 
-	fmt.Println(top) // prints top
-	printSlice(contents) // prints every element in slice contents line by line
-	fmt.Println(bottom) // prints bottom
+	fmt.Println(top)      // prints top
+	fmt.Println(contents) // prints every element in slice contents line by line
+	fmt.Println(bottom)   // prints bottom
 
 }
 
 // grabs the number with the correpsonding position in the matrix from the game state
-func (s *game.State) getNum(row int) string {
+func getNum(row int) string {
+	var s = game.State{}
 	var numbers []string
 	i := row
 	for j := 0; j < cols; j++ {
-		if s.board[i][j] == 0 {
-			numbers[j] == [" "]
+		if s.Board[i][j] == 0 {
+			numbers[j] = " "
 		}
-		numbers[j] = strconv.Itoa(s.board[i][j])
+		numbers[j] = strconv.Itoa(s.Board[i][j])
 	}
-	Box1 := " " + numbers[0] + " " + style.verticalColumnSeparator + " " + numbers[1] + " " + style.verticalColumnSeparator + " " + numbers[2] + " "
-	Box2 := style.verticalBoxSeparator + " " + numbers[3] + " " + style.verticalColumnSeparator + " " + numbers[4] + " " + style.verticalColumnSeparator + " " + numbers[5] + " " 
-	Box3 := style.verticalBoxSeparator + " " + numbers[6] + " " + style.verticalColumnSeparator + " " + numbers[7] + " " + style.verticalColumnSeparator + " " + numbers[8] + " "
-	var completerow string = style.verticalBoxSeparator + Box1 + Box2 + Box3 + style.verticalBoxSeparator
+	Box1 := " " + numbers[0] + " " + verticalColumnSeparator + " " + numbers[1] + " " + verticalColumnSeparator + " " + numbers[2] + " "
+	Box2 := verticalBoxSeparator + " " + numbers[3] + " " + verticalColumnSeparator + " " + numbers[4] + " " + verticalColumnSeparator + " " + numbers[5] + " "
+	Box3 := verticalBoxSeparator + " " + numbers[6] + " " + verticalColumnSeparator + " " + numbers[7] + " " + verticalColumnSeparator + " " + numbers[8] + " "
+	var completerow string = verticalBoxSeparator + Box1 + Box2 + Box3 + verticalBoxSeparator
 	return completerow
-}
-
-// gets all elements in slice through recursion
-func printSlice(s []int) {
-    fmt.Println(s[0])
-    printSlice(s[1:])
 }
--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"fmt"
 
-	_ "github.com/tehbooom/sudoku/board"
-	_ "github.com/tehbooom/sudoku/game"
+	"github.com/tehbooom/sudoku/game"
+	_ "github.com/tehbooom/sudoku/style"
 
 	"github.com/mbndr/figlet4go"
 )
 
 func main() {
-
+	var s = game.State{}
 	// generates the sudoku banner
 	// 				     #         #
 	//  mmm   m   m   mmm#   mmm   #   m  m   m
@@ -21,36 +21,37 @@ func main() {
 	renderStr, _ := ascii.Render("suGOku")
 	fmt.Print(renderStr)
 
-	game.Start()
+	s.Start()
+	// board.PrintBoard()
 
 	// start infinite game loop
-	for {
+	// for {
 
-		board.PrintBoard()
-		fmt.Println("Please enter the row column and value to put on the board..\n")
-		fmt.Println("For example, r1 c2 9")
-		for {
-			var row, column, value int
-			fmt.Scan(&row, &column, &value)
-			valid := game.placeNumber(row, column, value)
+	// 	board.PrintBoard()
+	// 	fmt.Println("Please enter the row column and value to put on the board..\n")
+	// 	fmt.Println("For example, r1 c2 9")
+	// 	// for {
+	// 	// 	var row, column, value int
+	// 	// 	fmt.Scan(&row, &column, &value)
+	// 	// 	valid := game.placeNumber(row, column, value)
 
-			if valid == nil {
-				break
-			}
-			fmt.Println(valid)
-			fmt.Printf("Please re-enter a correct position and value...\n")
-		}
+	// 	// 	if valid == nil {
+	// 	// 		break
+	// 	// 	}
+	// 	// 	fmt.Println(valid)
+	// 	// 	fmt.Printf("Please re-enter a correct position and value...\n")
+	// 	// }
 
-		result = board.checkForWinner()
-		if result == completeWrong {
-			fmt.Println("Somethings not right check again..")
-		}
-		if result == winner {
-			break
-		}
-		if result == incomplete {
-			fmt.Println()
-		}
-	}
+	// 	// result = board.checkForWinner()
+	// 	// if result == completeWrong {
+	// 	// 	fmt.Println("Somethings not right check again..")
+	// 	// }
+	// 	// if result == winner {
+	// 	// 	break
+	// 	// }
+	// 	// if result == incomplete {
+	// 	// 	fmt.Println()
+	// 	// }
+	// }
 
 }
