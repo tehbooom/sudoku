22bc1472f948fdcb242a71054860bdb9c2a40d87
Alec Carpenter <57996746+tehbooom@users.noreply.github.com>
2023-03-20 13:32:14 -0400

    Working game
    * i suk
    
    * we hawt
    
    * we hawt
    
    * All done
    
    * Better Readme

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/tehbooom/sudoku
 
 go 1.18
 
-require github.com/mbndr/figlet4go v0.0.0-20190224160619-d6cef5b186ea
+require (
+	github.com/aybabtme/rgbterm v0.0.0-20170906152045-cc83f3b3ce59
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
+	github.com/inancgumus/screen v0.0.0-20190314163918-06e984b86ed3
+)
+
+require (
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/term v0.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,53 +2,63 @@ package main
 
 import (
 	"fmt"
+	"os"
 
-	_ "github.com/tehbooom/sudoku/board"
-	_ "github.com/tehbooom/sudoku/game"
+	"github.com/inancgumus/screen"
 
-	"github.com/mbndr/figlet4go"
+	"github.com/tehbooom/sudoku/game"
+	_ "github.com/tehbooom/sudoku/style"
 )
 
 func main() {
+	var s = game.State{}
+	var difficulty int
+	arg_len := len(os.Args[1:])
+	if arg_len != 1 {
+		fmt.Println(" Please add arguements of either easy, medium, hard, expert")
+		os.Exit(1)
+	} else if os.Args[1] == "easy" {
+		difficulty = 20
+	} else if os.Args[1] == "medium" {
+		difficulty = 40
+	} else if os.Args[1] == "hard" {
+		difficulty = 60
+	} else if os.Args[1] == "expert" {
+		difficulty = 70
+	} else {
+		fmt.Println(" Please add arguements of either easy, medium, hard, expert")
+		os.Exit(1)
+	}
 
-	// generates the sudoku banner
-	// 				     #         #
-	//  mmm   m   m   mmm#   mmm   #   m  m   m
-	// #   "  #   #  #" "#  #" "#  # m"   #   #
-	//  """m  #   #  #   #  #   #  #"#    #   #
-	// "mmm"  "mm"#  "#m##  "#m#"  #  "m  "mm"#
-	ascii := figlet4go.NewAsciiRender()
-	renderStr, _ := ascii.Render("suGOku")
-	fmt.Print(renderStr)
-
-	game.Start()
+	s.Start(difficulty)
 
 	// start infinite game loop
 	for {
-
-		board.PrintBoard()
-		fmt.Println("Please enter the row column and value to put on the board..\n")
-		fmt.Println("For example, r1 c2 9")
+		screen.Clear()
+		screen.MoveTopLeft()
+		s.PrintBoard()
+		fmt.Println("Please enter the row column and value to put on the board: 1 2 9")
 		for {
 			var row, column, value int
 			fmt.Scan(&row, &column, &value)
-			valid := game.placeNumber(row, column, value)
+			valid := s.PlaceNumber(row, column, value)
 
 			if valid == nil {
 				break
 			}
 			fmt.Println(valid)
-			fmt.Printf("Please re-enter a correct position and value...\n")
 		}
 
-		result = board.checkForWinner()
-		if result == completeWrong {
+		result := s.CheckForWinner()
+		if result == "completeWrong" {
 			fmt.Println("Somethings not right check again..")
 		}
-		if result == winner {
+		if result == "winner" {
+			s.PrintBoard()
+			fmt.Println("Congrats!! You won suGOku")
 			break
 		}
-		if result == incomplete {
+		if result == "incomplete" {
 			fmt.Println()
 		}
 	}
