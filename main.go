20ef60c2f2445a9f0743d4b145b6568f8f59d950
tehbooom <onlyboomm@gmail.com>
2023-03-19 23:35:52 -0400

    we hawt

--- main.go
+++ main.go
@@ -11,47 +11,40 @@ import (
 
 func main() {
 	var s = game.State{}
-	// generates the sudoku banner
-	// 				     #         #
-	//  mmm   m   m   mmm#   mmm   #   m  m   m
-	// #   "  #   #  #" "#  #" "#  # m"   #   #
-	//  """m  #   #  #   #  #   #  #"#    #   #
-	// "mmm"  "mm"#  "#m##  "#m#"  #  "m  "mm"#
 	ascii := figlet4go.NewAsciiRender()
 	renderStr, _ := ascii.Render("suGOku")
 	fmt.Print(renderStr)
 
-	s.Start()
-	// board.PrintBoard()
+	s.Start(30)
 
 	// start infinite game loop
-	// for {
+	for {
 
-	// 	board.PrintBoard()
-	// 	fmt.Println("Please enter the row column and value to put on the board..\n")
-	// 	fmt.Println("For example, r1 c2 9")
-	// 	// for {
-	// 	// 	var row, column, value int
-	// 	// 	fmt.Scan(&row, &column, &value)
-	// 	// 	valid := game.placeNumber(row, column, value)
+		s.PrintBoard()
+		fmt.Println("Please enter the row column and value to put on the board..")
+		fmt.Println("For example, r1 c2 9")
+		for {
+			var row, column, value int
+			fmt.Scan(&row, &column, &value)
+			valid := s.placeNumber(row, column, value)
 
-	// 	// 	if valid == nil {
-	// 	// 		break
-	// 	// 	}
-	// 	// 	fmt.Println(valid)
-	// 	// 	fmt.Printf("Please re-enter a correct position and value...\n")
-	// 	// }
+			if valid == nil {
+				break
+			}
+			fmt.Println(valid)
+			fmt.Printf("Please re-enter a correct position and or value...\n")
+		}
 
-	// 	// result = board.checkForWinner()
-	// 	// if result == completeWrong {
-	// 	// 	fmt.Println("Somethings not right check again..")
-	// 	// }
-	// 	// if result == winner {
-	// 	// 	break
-	// 	// }
-	// 	// if result == incomplete {
-	// 	// 	fmt.Println()
-	// 	// }
-	// }
+		// result = board.checkForWinner()
+		// if result == completeWrong {
+		// 	fmt.Println("Somethings not right check again..")
+		// }
+		// if result == winner {
+		// 	break
+		// }
+		// if result == incomplete {
+		// 	fmt.Println()
+		// }
+	}
 
 }
